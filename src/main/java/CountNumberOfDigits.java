02527549245fb7880031cb8b898d3f9f1ed72566
Moshiur <moshi7403@gmail.com>
2021-06-13 20:23:45 -0700

    New problem and solution added

---
+++ src/main/java/CountNumberOfDigits.java
@@ -0,0 +1,15 @@
+public class CountNumberOfDigits {
+
+    public static void main(String[] args) {
+
+        int num =123456;
+        int count =0 ;
+
+        while (num>0){
+
+            num = num/10;
+            count++;
+        }
+        System.out.println("The total number of counts : " + count);
+    }
+}
