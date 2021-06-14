393f7075a9b7b90b5be1a001d5568d4341b552d7
Moshiur <moshi7403@gmail.com>
2021-06-13 21:04:52 -0700

    New problem and solution added

---
+++ src/main/java/CountSumOfDigits.java
@@ -0,0 +1,17 @@
+public class CountSumOfDigits {
+
+    public static void main(String[] args) {
+
+        int num = 1234;
+        int sum =0;
+
+        while(num>0){ // 12 1 0
+
+            sum = sum + num%10; // 4+3*2*1
+
+            num = num/10; // 123 12 1 0
+        }
+
+        System.out.println("Total sum is : " + sum);
+    }
+}
