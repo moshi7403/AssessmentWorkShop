b688710ec4379e16a92de6417f3644cbe74bab8d
Moshiur <moshi7403@gmail.com>
2021-12-20 13:04:57 -0700

    updated git ignore and added a new class

---
+++ src/main/java/MouseHovering.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class MouseHovering {
+
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        int x=100,y=400;
+        boolean bool=true;
+        Robot bot=new Robot();
+        while(true) {
+            bot.mouseMove(x, y);
+            if(bool) {
+                x+=5;y+=5;
+            }else {
+                x+=5;y-=5;
+            }
+            if(y==420)bool=false;
+            else if(y==380)bool=true;
+            if(x==1300)x=100;
+            Thread.sleep(20);
+        }
+    }
+}
